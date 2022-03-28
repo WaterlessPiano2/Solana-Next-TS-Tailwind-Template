5f65f26679932924faeba7013924a273578ac4ef
Chad Laptop <scderya@gmail.com>
2022-03-28 19:43:01 +0100

    button visible now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { WalletConnectButton } from "@solana/wallet-adapter-react-ui";
 import { SendOneLamportToRandomAddress } from "../components/Send";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1 className="text-xl font-bold">Hello Next.js + TS + Tailwind CSS👋!</h1>
     <div>
-      <WalletMultiButton />
-      <SendOneLamportToRandomAddress />
+      <WalletConnectButton className="btn hover:bg-violet-600 bg-violet-800 text-white disabled:bg-purple-400" />
+      <div className="btn hover:bg-violet-600 bg-violet-800 text-white disabled:bg-purple-400 p-4 mt-4 rounded-md">
+        <SendOneLamportToRandomAddress />
+      </div>
     </div>
     <p>
       <Link href="/about">

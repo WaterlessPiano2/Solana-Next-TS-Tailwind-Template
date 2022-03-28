13c7de7d70b4e7f1d8a5968730bf1df5fcad0c6d
Chad Laptop <scderya@gmail.com>
2022-03-28 22:37:49 +0100

    Moved the wallet back to the top so it can be accessed from amywhere

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import Link from "next/link";
 import Layout from "../components/Layout";
-import { Wallet } from "../components/Wallet";
+import { SendOneLamportToRandomAddress } from "../components/Send";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1 className="text-xl font-bold">Hello Next.js + TS + Tailwind CSS👋!</h1>
     <div>
-      <Wallet />
+      <div>
+        <WalletMultiButton />
+      </div>
+      <div>
+        <SendOneLamportToRandomAddress />
+      </div>{" "}
     </div>
     <p>
       <Link href="/about">
